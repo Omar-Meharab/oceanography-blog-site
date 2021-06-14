50434fe8157c543a64684b83cf6719dbcd1b8a52
omar meharab <omarmeharab81@gmail.com>
2021-06-14 17:25:54 +0600

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Demo from './components/demo/demo';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>this is new</h1>
+      <Demo></Demo>
     </div>
   );
 }
